910178142e27071881e7d20efdcfe4ddfc4e21f9
victoriahu <vh12490@gmail.com>
2018-11-28 22:00:13 -0800

    "got to containers"

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,7 +2,19 @@ import React from 'react';
 import Search from './../components/Search.js';
 import { connect } from 'react-redux';
 import handleSearchChange from '../actions/search.js';
-var SearchContainer = () => {};
+
+
+
+var SearchContainer = () => {
+
+//Search.js is our component that we want to connect to
+//handleSearchChange is our dispatch action creator
+//mapStateToProps... handleInputChange accepts state
+  connect(Search.handleInputChange, handleSearchChange)(Search);
+
+};
+
+
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
