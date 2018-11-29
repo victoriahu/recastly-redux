ccc85a64fee3b2430863f96acc92d4aa1e5c5236
victoriahu <vh12490@gmail.com>
2018-11-28 21:23:02 -0800

    "add store"

--- src/store/store.js
+++ src/store/store.js
@@ -3,3 +3,15 @@ import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+
+// export default function configureStore(initialState) {
+//     return createStore(
+//         rootReducer,
+//         initialState,
+//         applyMiddleware(thunk)
+//     );
+// }
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
